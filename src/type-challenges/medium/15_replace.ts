c98e8752a7788cca94de0a337b1f05d8f60ac65c
eduard.hannoshyn <eduard.hannoshyn@avenga.com>
2023-04-11 16:31:02 +0300

    resolved new task

---
+++ src/type-challenges/medium/15_replace.ts
@@ -0,0 +1,43 @@
+/*
+  116 - Replace
+  -------
+  by Anthony Fu (@antfu) #medium #template-literal
+
+  ### Question
+
+  Implement `Replace<S, From, To>` which replace the string `From` with `To` once in the given string `S`
+
+  For example
+
+  ```ts
+  type replaced = Replace<'types are fun!', 'fun', 'awesome'> // expected to be 'types are awesome!'
+  ```
+
+  > View on GitHub: https://tsch.js.org/116
+*/
+
+/* _____________ Your Code Here _____________ */
+
+
+// type Replace<S extends string, From extends string, To extends string> = From extends "" ? S : S extends `${infer F}${From}${infer R}` ? `${F}${To}${R}` : S;
+type Replace<S extends string, From extends string, To extends string> = S extends `${infer F}${From}${infer R}` ? `${F}${From extends "" ? From : To}${R}` : S;
+// foobar    bar
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+// @ts-ignore
+type cases = [
+    Expect<Equal<Replace<'foobar', 'bar', 'foo'>, 'foofoo'>>,
+    Expect<Equal<Replace<'foobarbar', 'bar', 'foo'>, 'foofoobar'>>,
+    Expect<Equal<Replace<'foobarbar', '', 'foo'>, 'foobarbar'>>,
+    Expect<Equal<Replace<'foobarbar', 'bar', ''>, 'foobar'>>,
+    Expect<Equal<Replace<'foobarbar', 'bra', 'foo'>, 'foobarbar'>>,
+    Expect<Equal<Replace<'', '', ''>, ''>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/116/answer
+  > View solutions: https://tsch.js.org/116/solutions
+  > More Challenges: https://tsch.js.org
+*/
